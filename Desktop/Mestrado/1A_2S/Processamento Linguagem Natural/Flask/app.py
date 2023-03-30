368d0c2a6e12c7b8d89409e64944516a407566f8
Tomás Lima <122676618+Tomaicho@users.noreply.github.com>
2023-03-31 00:19:42 +0100

    Flask project added

---
+++ Desktop/Mestrado/1A_2S/Processamento Linguagem Natural/Flask/app.py
@@ -0,0 +1,27 @@
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static')
+
+@app.route("/tomacho-home")
+def homepage():
+    return render_template('homePage.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/projects/3Es")
+def tresEs():
+    return render_template('3EsPLN.html')
+
+@app.route("/layout")
+def layout():
+    return render_template('layout.html')
+
+# @app.route("/api/termos")
+# def termos_api():
+#     jsonfile = json.load(dbFile)
+#     return json.dump(jsonfile)
+
+app.run(host="localhost", port=4002, debug=True)
